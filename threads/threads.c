49da87cad5b434f5e3081201f89727fedabcb8ce
Ian Ballou <ianballou67@gmail.com>
2017-10-19 05:38:10 +0000

    currently debugging stack bugs:

--- threads/threads.c
+++ threads/threads.c
@@ -20,7 +20,8 @@
 static struct pthread {
     pthread_t id;
     jmp_buf env;        // Environment variables
-    int (*function)();  // Thread function pointer
+    //int (*function)();  // Thread function pointer
+    int function;  // Thread function pointer
     unsigned long *stack_store;  // 4 bytes?
     int stack_addr;
     int state;
@@ -34,9 +35,11 @@ static struct pthread {
 jmp_buf root_env;
 int num_threads = -1;
 pthread_t cur_thread = 0;
+pthread_t last_thread = 0;
 int need_sched = 0;
 int need_setup = 1;
 struct sigaction act;
+static int thread_map[MAX_THREADS] = {0};
 
 /* Copy jmp_buf context */
 void copy_context(jmp_buf s, jmp_buf d)
@@ -66,7 +69,7 @@ void schedule()
             }
             cur_thread++;
         }
-        if (pthreads[cur_thread].needs_setup) {
+        if (pthreads[cur_thread].needs_setup == 1) {
 			pthreads[cur_thread].env[0].__jmpbuf[PCIDX] =
 				pthreads[cur_thread].function;
         	pthreads[cur_thread].env[0].__jmpbuf[SPIDX] =
@@ -109,15 +112,16 @@ void setup_threads()
     }*/
     act.sa_handler = interrupt;
     act.sa_flags = SA_NODEFER;
-    sigaction (SIGALRM, &act, 0);
-    ualarm(ALRM_TIME, 0);
 
     // pthreads[0] is main thread
     pthreads[0].id = 0;
     pthreads[0].stack_store = NULL;
     pthreads[0].state = READY;
+    pthreads[0].needs_setup = 1;
 
     setjmp(pthreads[0].env); // ready root thread
+    sigaction (SIGALRM, &act, 0);
+    ualarm(ALRM_TIME, 0);
 	schedule();
 }
 
@@ -131,21 +135,34 @@ int pthread_create(
 	void *(*start_routine) (void *), 
 	void *arg) 
 {
-    int temp_thread = 1; // TODO need to replace with search
-    *thread = temp_thread;
-    unsigned long *stack_space;
+    int temp_thread;
+    // get new id
+    int i = last_thread;
+    while (1) {
+       if (i >= MAX_THREADS) {
+           i = 0;
+       } 
 
-    int tid = num_threads++; // XXX need to replace
+       if (thread_map[i] == 0) {
+           temp_thread = i;
+           break ;
+       }
+    }
+    int tid = temp_thread;
+    last_thread = temp_thread;
+    *thread = temp_thread;
+    //unsigned long *stack_space;
+
+    num_threads++; // XXX need to replace
     pthreads[temp_thread].id = tid;
 
     // stack it up
-    stack_space = malloc(sizeof(int)*STACK_SIZE/4);
-    if (stack_space == NULL) return -1;
-    pthreads[temp_thread].stack_store = stack_space;
-    pthreads[temp_thread].stack_store[(STACK_SIZE / 4) - 1] 
-        = (unsigned long)arg;
-    pthreads[temp_thread].stack_store[(STACK_SIZE / 4) - 2] 
+    //if (stack_space == NULL) return -1;
+    pthreads[temp_thread].stack_store = malloc(sizeof(int)*STACK_SIZE/4);
+    pthreads[temp_thread].stack_store[STACK_SIZE / 4 - 2] 
         = (unsigned int)kill_thread;
+    pthreads[temp_thread].stack_store[STACK_SIZE / 4 - 1] 
+        = (unsigned long int)arg;
     
     // reg it up
     pthreads[temp_thread].stack_addr = ptr_mangle(
@@ -172,6 +189,13 @@ int pthread_create(
 void pthread_exit(void *value_ptr)
 {
     num_threads--;
+    thread_map[cur_thread] = 0;
+    sigaction (SIGALRM, &act, 0);
+    ualarm(0, 0); // turn off alarm temporarily
+
+    pthreads[cur_thread].state = EXITED;
+    schedule();
+    __builtin_unreachable();
 }
 
 pthread_t pthread_self(void)
